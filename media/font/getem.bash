e6605d18ebc26591c028b9645f6b9cb75014e0a9
Derek Rhodes <physci@gmail.com>
2025-02-26 10:30:21 -0500

    adds computer modern font

---
+++ media/font/getem.bash
@@ -0,0 +1,4 @@
+ wget 'http://spratt.github.io/Computer-Modern/cmunss.otf'
+ wget 'http://spratt.github.io/Computer-Modern/cmunsx.otf'
+ wget 'http://spratt.github.io/Computer-Modern/cmunsi.otf'
+ wget 'http://spratt.github.io/Computer-Modern/cmunbxo.otf'
